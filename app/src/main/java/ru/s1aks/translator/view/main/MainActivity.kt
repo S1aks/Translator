d960c6c297e14fc01ed0d7ffc0e2f6fa3250d22d
Viacheslav Khrykin <vh78aks@gmail.com>
2021-11-24 21:00:59 +0300

    Initial commit

---
+++ app/src/main/java/ru/s1aks/translator/view/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.s1aks.translator.view.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.s1aks.translator.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
